998666d4674715b41d4c845cb24b7e950d32a12d
pulcovamon <pulcovamon@gmail.com>
2024-07-27 20:15:19 +0200

    move change state function to the right place

--- api/worker.py
+++ api/worker.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.signals import after_task_publish
 
 # celery broker and backend urls
 CELERY_BROKER_URL = os.getenv("REDIS_URL", "redis://redis_server:6379")
@@ -10,6 +11,16 @@ CELERY_RESULT_BACKEND = os.getenv("REDIS_URL", "redis://redis_server:6379")
 # create celery application
 celery_app = Celery(
     "celery",
-    backend=CELERY_BROKER_URL,
-    broker=CELERY_RESULT_BACKEND,
+    backend=CELERY_RESULT_BACKEND,
+    broker=CELERY_BROKER_URL,
 )
+
+@after_task_publish.connect
+def update_sent_state(sender=None, headers=None, **kwargs):
+    """
+    Set celery task state to 'SENT' if in process.
+    """
+    backend = (
+        task.backend if celery_app.tasks.get(sender) else celery_app.backend
+    )
+    backend.store_result(headers["id"], None, "SENT")
--- worker/worker.py
+++ worker/worker.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.signals import after_task_publish
 
 # celery broker and backend urls
 CELERY_BROKER_URL = os.getenv("REDIS_URL", "redis://redis_server:6379")
@@ -13,3 +14,13 @@ celery_app = Celery(
     backend=CELERY_RESULT_BACKEND,
     broker=CELERY_BROKER_URL,
 )
+
+@after_task_publish.connect
+def update_sent_state(sender=None, headers=None, **kwargs):
+    """
+    Set celery task state to 'SENT' if in process.
+    """
+    backend = (
+        task.backend if celery_app.tasks.get(sender) else celery_app.backend
+    )
+    backend.store_result(headers["id"], None, "SENT")
