9febb0b2200b17845da0f6550c2a1a70bf1fb5a9
pulcovamon <pulcovamon@gmail.com>
2024-08-14 18:26:34 +0200

    add footer and cook with css

--- frontend/src/lib/pages/Layout.tsx
+++ frontend/src/lib/pages/Layout.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function Layout() {
   return (
     <div className="homepage">
       <Navbar />
-      <Outlet />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 }
