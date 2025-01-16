0f027150804f16997a22ac0024d52a21b6bdf6de
pulcovamon <pulcovamon@gmail.com>
2025-01-16 12:10:03 +0100

    mark current location

--- frontend/src/lib/components/Navbar.tsx
+++ frontend/src/lib/components/Navbar.tsx
@@ -8,10 +8,12 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useState } from "react";
 import "./navbar.css";
+import { useLocation } from "react-router-dom";
 
 export default function Navbar() {
   const [openMenus, setOpenMenus] = useState<Record<string, boolean>>({});
   const [collapsed, setCollapsed] = useState<boolean>(true);
+  const location = useLocation();
 
   useEffect(() => {
     if (collapsed) {
@@ -27,6 +29,13 @@ export default function Navbar() {
     }));
   };
 
+  function isActive(path: string) {
+    if (path === "/") {
+      return location.pathname === path;
+    }
+    return location.pathname.includes(path);
+  }
+
   return (
     <div className="navbar">
       <div className="collapse-button-box">
@@ -38,14 +47,21 @@ export default function Navbar() {
         </button>
       </div>
       <ul className="outer-list">
-        <li>
+        <li className={isActive("/") ? "active" : undefined}>
           <a href="/">
             <FontAwesomeIcon icon={faHouse} />
             {collapsed ? <></> : <span>Home</span>}
           </a>
         </li>
 
-        <li>
+        <li
+          className={
+            (isActive("/score") || isActive("/result")) &&
+            !openMenus["scoring-system"]
+              ? "active"
+              : "passive"
+          }
+        >
           <div
             className="menu-button"
             onClick={() => toggleMenu("scoring-system")}
@@ -55,24 +71,31 @@ export default function Navbar() {
           </div>
           {openMenus["scoring-system"] && (
             <ul className="submenu">
-              <li>
+              <li className={isActive("/score") ? "active" : "passive"}>
                 <a href="/score">Import data</a>
               </li>
-              <li>
+              <li className={isActive("/result") ? "active" : "passive"}>
                 <a href="/result">History</a>
               </li>
             </ul>
           )}
         </li>
 
-        <li>
+        <li
+          className={
+            isActive("/catalog") &&
+            !openMenus["catalog"]
+              ? "active"
+              : "passive"
+          }
+          >
           <div className="menu-button" onClick={() => toggleMenu("catalog")}>
             <FontAwesomeIcon icon={faAddressBook} />
             {collapsed ? <></> : <span>Training data</span>}
           </div>
           {openMenus["catalog"] && (
             <ul className="submenu">
-              <li>
+              <li className={isActive("/catalog") ? "active" : "passive"}>
                 <a href="/catalog">Lung cancer</a>
               </li>
             </ul>
