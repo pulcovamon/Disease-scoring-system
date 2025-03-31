1767a3bfa4b6a091cbf33bbdec3a7662831c40e0
pulcovamon <pulcovamon@gmail.com>
2025-03-31 20:16:04 +0200

    make possible to get also default models and all public models

--- api/models/endpoints.py
+++ api/models/endpoints.py
@@ -1,11 +1,11 @@
 import os
 from typing import Optional
 
-from fastapi import APIRouter, UploadFile, HTTPException, File, Depends
+from fastapi import APIRouter, UploadFile, HTTPException, File, Depends, Query
 from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 from bson import ObjectId
+from bson.json_util import dumps
 
 from api.database import MongoDatabase
 from api.models.utils import sanitize_filename
@@ -18,22 +18,25 @@ class ModelUploadForm(BaseModel):
     username: str
     model_name: str
     description: str = ""
+    is_public: bool = False
 
     @classmethod
     def as_form(
         cls,
         username: str = File(...),
         model_name: str = File(...),
-        description: str = File("")
+        description: str = File(""),
+        is_public: bool = File(False)
     ):
-        return cls(username=username, model_name=model_name, description=description)
+        return cls(username=username, model_name=model_name, description=description, is_public=is_public)
 
 
 @router.post("/model")
 async def upload_model(
     form: ModelUploadForm = Depends(ModelUploadForm.as_form),
     file: UploadFile = File(...),
-    image: Optional[UploadFile] = File(None)
+    image: Optional[UploadFile] = File(None),
+    encoder: Optional[UploadFile] = File(None)
 ):
     ext = os.path.splitext(file.filename)[1].lower()
     if ext != ".pkl":
@@ -49,6 +52,7 @@ async def upload_model(
         f.write(content)
 
     image_filename = None
+    image_path = None
     if image:
         img_ext = os.path.splitext(image.filename)[1].lower()
         if img_ext not in [".jpg", ".jpeg", ".png", ".webp"]:
@@ -57,40 +61,32 @@ async def upload_model(
         with open(image_path, "wb") as f:
             img_content = await image.read()
             f.write(img_content)
-        image_filename = image.filename
+        image_filename = image_path
+
+    encoder_path = None
+    if encoder:
+        encoder_ext = os.path.splitext(encoder.filename)[1].lower()
+        if encoder_ext != ".pkl":
+            raise HTTPException(status_code=422, detail="Unsupported encoder format. Expected .pkl file.")
+        encoder_path = os.path.join(user_dir, sanitize_filename(encoder.filename))
+        with open(encoder_path, "wb") as f:
+            enc_content = await encoder.read()
+            f.write(enc_content)
 
     model_entry = {
-        "filename": file.filename,
+        "user": form.username,
+        "path": model_path,
         "name": form.model_name,
         "description": form.description,
-        "image": image_filename
+        "image": image_filename,
+        "is_public": form.is_public,
+        "encoder": encoder_path
     }
 
-    existing_doc = models_db.collection.find_one({"user": form.username})
-    if existing_doc:
-        models_db.collection.update_one(
-            {"user": form.username},
-            {"$push": {"children": model_entry}}
-        )
-        model_id = existing_doc["_id"]
-    else:
-        result = models_db.collection.insert_one({
-            "user": form.username,
-            "path": user_dir,
-            "children": [model_entry]
-        })
-        model_id = result.inserted_id
+    result = models_db.collection.insert_one(model_entry)
+    model_id = result.inserted_id
 
     return {"status": "Model uploaded", "model_id": str(model_id)}
-
-
-@router.get("/model")
-async def get_models(username: str):
-    data = models_db.collection.find_one({"user": username})
-    if not data:
-        return JSONResponse(content={"models": []}, status_code=200)
-    return JSONResponse(content=jsonable_encoder(data["children"]), status_code=200)
-
 
 @router.get("/model/{_id}")
 async def get_model_by_id(_id: str):
@@ -103,4 +99,35 @@ async def get_model_by_id(_id: str):
     if not data:
         raise HTTPException(status_code=404, detail="Model not found")
 
-    return JSONResponse(content=jsonable_encoder(data), status_code=200)
+    return JSONResponse(content=dumps(data), status_code=200)
+
+@router.get("/model")
+async def get_models(
+    username: Optional[str] = Query(None),
+    include_public: bool = Query(False),
+    include_default: bool = Query(False)
+):
+    filters = []
+
+    if username:
+        filters.append({"user": username})
+
+    if include_default:
+        filters.append({"user": "default"})
+
+    if include_public:
+        filters.append({"is_public": True})
+
+    if not filters:
+        raise HTTPException(status_code=400, detail="No filters specified.")
+
+    query = {"$or": filters} if len(filters) > 1 else filters[0]
+    raw_models = list(models_db.collection.find(query))
+
+    unique_models = {}
+    for model in raw_models:
+        model["_id"] = str(model["_id"])
+        unique_models[model["_id"]] = model
+
+    return JSONResponse(content=list(unique_models.values()), status_code=200)
+
